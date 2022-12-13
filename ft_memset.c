5d1d70deac0cbc79d1f6871150d7f38f748288fb
Yassine-Zaazaa <Yassine.zaazaa@outlook.com>
2022-12-13 17:14:41 +0100

    memset

---
+++ ft_memset.c
@@ -0,0 +1,13 @@
+#include "libft.h"
+
+void	*memset(void *s, int c, size_t n)
+{
+	unsigned char	*str;
+	size_t		i;
+
+	i = 0;
+	str = (unsigned char *)s;
+	while(i < n)
+		str[i++] = c;
+	return (str);
+}
