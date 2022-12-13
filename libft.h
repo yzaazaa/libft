dcef055ab88e793b68936b0afedfd0e9cf240bea
Yassine-Zaazaa <Yassine.zaazaa@outlook.com>
2022-12-13 18:01:12 +0100

    added memmove

--- libft.h
+++ libft.h
@@ -9,5 +9,6 @@ void	ft_bzero(void *s, size_t n);
 void 	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 
 #endif
