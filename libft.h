e210e3961e68ddb74e33c16b4e58c2299e4c353e
Yassine-Zaazaa <Yassine.zaazaa@outlook.com>
2022-12-14 15:32:44 +0100

    added ft_isalpha

--- libft.h
+++ libft.h
@@ -25,5 +25,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int	ft_strcmp(const char *s1, const char *s2);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
 int	ft_atoi(const char *nptr);
+int	ft_isalpha(int c);
+
 
 #endif
