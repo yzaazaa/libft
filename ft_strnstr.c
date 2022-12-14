4a2ec142ee812d5f12f45388a537d9041e4cc531
Yassine-Zaazaa <Yassine.zaazaa@outlook.com>
2022-12-14 14:53:45 +0100

    added ft_strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,21 @@
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+
+	if(!needle)
+		return ((char *)haystack);
+	while(*haystack && len-- > 0)
+	{
+		i = 0;
+		while(haystack[i] == needle[i] i < len + 1)
+		{
+			i++;
+			if(needle[j] == '\0')
+				return ((char *)haystack);
+		}
+		haystack++;
+	}
+	return (NULL);
+}
--- libft.h
+++ libft.h
@@ -21,6 +21,6 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strstr(const char *haystack, const char *needle);
-
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 
 #endif
