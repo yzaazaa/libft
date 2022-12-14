9b6cd30ecf3825ef4cfb6df4b7faaffc0247c246
Yassine-Zaazaa <Yassine.zaazaa@outlook.com>
2022-12-14 12:27:29 +0100

    added ft_strncpy

--- libft.h
+++ libft.h
@@ -14,5 +14,6 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s);
 char	*ft_strcpy(char *dest, const char *src);
+char	*ft_strncpy(char *dest, const char *src, size_t n);
 
 #endif
