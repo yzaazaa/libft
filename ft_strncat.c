9d48a94d640b6d234402370a0113e4bd44c509c4
Yassine-Zaazaa <Yassine.zaazaa@outlook.com>
2022-12-14 12:59:02 +0100

    added ft_strncat

---
+++ ft_strncat.c
@@ -0,0 +1,21 @@
+#include "libft.h"
+
+char	*ft_strncat(char *dest, const char *src, size_t n)
+{
+	size_t	i;
+	size_t	j;
+
+	if(!dest)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while(dest[i++]);
+	while(src[j] && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
--- libft.h
+++ libft.h
@@ -16,5 +16,7 @@ char	*ft_strdup(const char *s);
 char	*ft_strcpy(char *dest, const char *src);
 char	*ft_strncpy(char *dest, const char *src, size_t n);
 char	*ft_strcat(char *dest, const char *src);
+char	*ft_strncat(char *dest, const char *src, size_t n);
+
 
 #endif
