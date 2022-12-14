a2b39dd41b858329a689b868ea46ae336564fc60
Yassine-Zaazaa <Yassine.zaazaa@outlook.com>
2022-12-14 17:36:45 +0100

    added ft_putendl

--- libft.h
+++ libft.h
@@ -37,7 +37,7 @@ void	ft_putstr_fd(const char *s, int fd);
 void	ft_putendl_fd(const char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putnbr(int n);
-
+void	ft_putendl(const char *s);
 
 
 
