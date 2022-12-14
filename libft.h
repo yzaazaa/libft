88f0dc759aa505f631d624544a30929d98e91a74
Yassine-Zaazaa <Yassine.zaazaa@outlook.com>
2022-12-14 15:34:54 +0100

    added ft_isdigit

--- libft.h
+++ libft.h
@@ -26,6 +26,8 @@ int	ft_strcmp(const char *s1, const char *s2);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
 int	ft_atoi(const char *nptr);
 int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+
 
 
 #endif
