200bf7414b80066ae4253cb146fd1585a4e65cd7
Yassine-Zaazaa <Yassine.zaazaa@outlook.com>
2022-12-14 17:34:05 +0100

    added ft_putnbr

--- libft.h
+++ libft.h
@@ -36,5 +36,9 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(const char *s, int fd);
 void	ft_putendl_fd(const char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+void	ft_putnbr(int n);
+
+
+
 
 #endif
