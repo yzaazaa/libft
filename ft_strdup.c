0f5e4a4a26b78a19f76a36c1e84d992a43ada3ae
Yassine-Zaazaa <Yassine.zaazaa@outlook.com>
2022-12-15 17:29:57 +0100

    add ft_strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,8 +6,10 @@ char	*ft_strdup(const char *s)
 	int	i;
 
 	i = 0;
-	str = malloc(sizeof(int) * (ft_strlen(s) + 1));
-	if(!s || !str)
+	if(!s)
+		return (NULL);
+	str = malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if(!str)
 		return (NULL);
 	while(s[i])
 	{
