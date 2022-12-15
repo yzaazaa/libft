10b9580c359919bcbedcabbe5b57eb652e35510d
Yassine-Zaazaa <Yassine.zaazaa@outlook.com>
2022-12-15 17:31:22 +0100

    add ft_substr

--- ft_substr.c
+++ ft_substr.c
@@ -3,20 +3,21 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*ret;
-	size_t	i;
+	size_t	s_len;
 
 	if(!s)
 		return (NULL);
-	if(ft_strlen(s) < start)
+	s_len = ft_strlen(s);
+	if(s_len <= start)
+	{	
 		len = 0;
+		start = s_len;
+	}
+	if(len > s_len - start)
+		len = s_len - start;
 	ret = malloc(sizeof(char) * (len + 1));
 	if(!ret)
 		return (NULL);
-	i = 0;
-	while(i < len)
-	{
-		ret[i++] = s[start++];	
-	}
-	ret[i] = '\0';
+	ft_strlcpy(ret, &s[start], len + 1);
 	return (ret);
 }
