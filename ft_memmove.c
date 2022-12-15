819afa138f13a58f1c296eb44420c9e8e7089fb7
Yassine-Zaazaa <Yassine.zaazaa@outlook.com>
2022-12-15 17:28:49 +0100

    add ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -2,21 +2,12 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	char 		*temp;
-	char 		*str;
-	const char 	*ptr;
-	size_t		i;
-
-	temp = malloc(sizeof(char) * n);
-	i = 0;
-	str = (char *)dest;
-	ptr = (const char *)src;
-	while(i < n)
-	{
-		temp[i] = ptr[i];
-		str[i] = temp[i];
-		i++;
-	}
-	free(temp);
+	if(!dest && !src)
+		return (dest);
+	if(src < dest)
+		while(n--)
+			*(char *)(dest + n) = *(char *)(src + n);
+	else if (src > dest)
+		ft_memcpy(dest, src, n);
 	return (dest);
 }
