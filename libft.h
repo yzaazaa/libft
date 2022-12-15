e14e09255e5811524e45d87125965792c74332a4
Yassine-Zaazaa <Yassine.zaazaa@outlook.com>
2022-12-15 14:01:58 +0100

    del ft_strlcpy

--- libft.h
+++ libft.h
@@ -15,6 +15,7 @@ size_t	ft_strlen(const char *s);
 char	*ft_strdup(const char *s);
 char	*ft_strcpy(char *dest, const char *src);
 char	*ft_strncpy(char *dest, const char *src, size_t n);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 char	*ft_strcat(char *dest, const char *src);
 char	*ft_strncat(char *dest, const char *src, size_t n);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
