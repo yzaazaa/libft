a3da809b7358c5795c08ea09ebfd9a6af2c370fb
Yassine-Zaazaa <Yassine.zaazaa@outlook.com>
2022-12-15 13:34:22 +0100

    added ft_strdel

--- libft.h
+++ libft.h
@@ -52,4 +52,6 @@ char	*ft_strtrim(char const *s);
 void	ft_striter(char *s, void (*f)(char *));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 void	ft_strclr(char *s);
+void	ft_strdel(char **as);
+
 #endif
