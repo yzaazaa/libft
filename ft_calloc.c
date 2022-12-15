77c60a0e8995f54770a0b6eb5761d833029af466
Yassine-Zaazaa <Yassine.zaazaa@outlook.com>
2022-12-15 15:05:43 +0100

    add ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -4,9 +4,9 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
 
-	p = malloc(nmemb * size);
-	if(!p)
+	ptr = malloc(nmemb * size);
+	if(!ptr)
 		return (NULL);
-	ft_bzero(p, nmemb * size);
-	return (p);
+	ft_bzero(ptr, nmemb * size);
+	return (ptr);
 }
