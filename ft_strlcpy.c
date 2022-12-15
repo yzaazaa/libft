5f07f70b16b61a5dc4b7d24d2de07695e9c123e8
Yassine-Zaazaa <Yassine.zaazaa@outlook.com>
2022-12-15 15:06:25 +0100

    add ft_strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,15 +1,15 @@
 #include "libft.h"
 
-size_t	ft_strncpy(char *dest, const char *src, size_t size)
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
 	size_t	len;
 	
 	len = ft_strlen(src);
 	if(!dest || !src || !size)
-		return (ftstrle);
+		return (len);
 	if(len >= size)
 		len = size - 1;
 	ft_memcpy(dest, src, len);
 	dest[len] = 0;
-	return (ft_strlen(src));
+	return (len);
 }
