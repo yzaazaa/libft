946eebd89b64e67a9d0e58a2221037169b35f922
Yassine-Zaazaa <Yassine.zaazaa@outlook.com>
2022-12-15 17:30:24 +0100

    add ft_strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -5,11 +5,11 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 	size_t	len;
 	
 	len = ft_strlen(src);
-	if(!dest || !src || !size)
+	if(!size)
 		return (len);
 	if(len >= size)
 		len = size - 1;
 	ft_memcpy(dest, src, len);
 	dest[len] = 0;
-	return (len);
+	return (ft_strlen(src));
 }
