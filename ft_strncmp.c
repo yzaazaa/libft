7ce52de5ac0c6927654f0f0e008c84df31db0340
Yassine-Zaazaa <Yassine.zaazaa@outlook.com>
2022-12-15 17:30:39 +0100

    add ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,10 +6,12 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	unsigned char	*str1;
 	unsigned char 	*str2;
 
+	if(!n)
+		return (0);
 	i = 0;
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
-	while(str1[i] && str2[i] && str1[i] == str2[i] && i < n)
+	while(str1[i] && str1[i] == str2[i] && --n)
 		i++;
 	return (str1[i] - str2[i]);
 }
