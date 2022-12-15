9d1bef887b381c34cb8d6da973a71052e432b414
Yassine-Zaazaa <Yassine.zaazaa@outlook.com>
2022-12-15 17:31:09 +0100

    add ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,24 +1,31 @@
 #include "libft.h"
 
-static int	ft_isblank(char c)
+static int	ft_str_chr(char c, char const *set)
 {
-	if(c == ' ' || c == '\n' || c == '\t')
-		return (1);
+	while(*set)
+	{
+		if(c == *set++)
+			return (1);
+	}
 	return (0);
 }
 
-char		*ft_strtrim(char const *s)
+char		*ft_strtrim(char const *s, char const *set)
 {
 	int	start;
 	int	i;
 	int	end;
 	char	*ret;	
 
+	if(!s || !set)
+		return (NULL);
 	i = 0;
-	while(ft_isblank(s[i++]));
+	while(s[i] && ft_str_chr(s[i], set))
+		i++;
 	start = i;
 	i = ft_strlen(s) - 1;
-	while(ft_isblank(s[i--]));
+	while(s[i] && ft_str_chr(s[i], set))
+		i--;
 	end = i;
 	ret = ft_substr(s, start, (end - start + 1));
 	return (ret);
