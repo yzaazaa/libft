1335a5d0ba2f2686317e93f88e1350a79c710bee
Yassine-Zaazaa <Yassine.zaazaa@outlook.com>
2022-12-16 12:31:53 +0100

    add ft_lstadd_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -2,6 +2,8 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
+	t_list	*last;
+
 	if(!lst)
 		return ;
 	if(!*lst)
@@ -9,8 +11,6 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 		*lst = new;
 		return ;
 	}
-	while((*lst)->next)
-		*lst = (*lst)->next;
-	(*lst)->next = new;
-	new->next = NULL;
+	last = ft_lstlast(*lst);
+	last->next = new;
 }
